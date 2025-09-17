51495fdb367dbf4436fa68926ee73d4cfd36edf1
BaptGau <baptiste.gautier@diabolocom.com>
2025-09-17 12:22:05 +0200

    fix

--- fonctions/TDD/exercice_tdd.py
+++ fonctions/TDD/exercice_tdd.py
@@ -1,1 +1,6 @@
 # Recoder la calculatrice en TDD (avec des tests en tout cas)
+# Exercice
+# Faire une fonction "calculatrice" qui prend en paramètres 3 éléments:
+# 2 nombres décimaux et une instruction ["add", "subtract", "multiply", "divide"]
+# la fonction return l'opération correspondante.
+# bonus: bien typé
