982c4b3cd80809c816ebd642cb1dbf13d67ccaad
BaptGau <baptiste.gautier@diabolocom.com>
2025-09-30 19:24:55 +0200

    enum correction

--- custom_types/enumeration/exercice_enum.py
+++ custom_types/enumeration/exercice_enum.py
@@ -1,5 +1,23 @@
 # Exercice enum:
 # Recoder la fonction calculatrice avec une instruction qui est un enum
+from enum import Enum, auto
 
-def calculatrice(x: int, y: int, operation: ...) -> int:
-    ...
+class Operation(Enum):
+    ADD = auto()
+    SUBTRACT = auto()
+    MULTIPLY = auto()
+    DIVIDE = auto()
+
+
+def calculatrice(x: int, y: int, operation: Operation) -> int | float:
+    match operation:
+        case Operation.ADD:
+            return x + y
+        case Operation.SUBTRACT:
+            return x - y
+        case Operation.MULTIPLY:
+            return x * y
+        case Operation.DIVIDE:
+            return x / y
+        case _:
+            raise ValueError(f"Unknown operation {operation}")
