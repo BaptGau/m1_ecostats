03b4521ca4c0c0763ec5efc83238b6e444b7040a
BaptGau <baptiste.gautier@diabolocom.com>
2025-09-30 19:02:24 +0200

    base

---
+++ base.py
@@ -0,0 +1,27 @@
+i = 10
+s = "bonjour"
+
+i = "10" # on peut changer un entier en chaine de caractères
+
+# opérations sur nombres
+n = 10 + 15.0
+print(n)
+n = 10 / 5
+print(n)
+n = 10 - 5
+print(n)
+
+# opérations sur chaines de caractères
+s = "hello"
+print(s)
+s2 = "world"
+print(s2)
+s3 = s+s2
+print(s3)
+
+# booleans
+b = True
+b2 = False
+b3 = True
+print(b == b2)
+print(b == b3)
---
+++ custom_types/enumeration/exercice_enum.py
@@ -0,0 +1,5 @@
+# Exercice enum:
+# Recoder la fonction calculatrice avec une instruction qui est un enum
+
+def calculatrice(x: int, y: int, operation: ...) -> int:
+    ...
